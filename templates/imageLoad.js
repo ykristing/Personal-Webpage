51fe32bb70a4d87713ee6ec0f8e7ad9227c25353
ykristing <y.kristi.ng@gmail.com>
2023-07-01 11:03:03 -0400

    images preload w/ js file

---
+++ templates/imageLoad.js
@@ -0,0 +1,15 @@
+var images = new Array();
+
+function preloadImages(){
+
+    for (i=0; i < preloadImages.arguments.length; i++){
+
+         images[i] = new Image();
+
+        images[i].src = preloadImages.arguments[i];
+
+    }
+
+}
+
+preloadImages("monstera1.png", "proj-imgs/audio.png", "proj-imgs/msp.png", "proj-imgs/nelson-fp.png", "proj-imgs/socioeconomic.png");
